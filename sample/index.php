fd42221815f47bc824413655a64c18368a778bf3
Oscar Barrett <oscar.barrett@league.agency>
2016-10-26 16:01:19 +0800

    Updated Dockerfile: - Use ubuntu:16.04, php7 - Removed php-apc and added php-intl - Removed unneeded call to enmod for mcrypt. - Allow override by default
    Removed Makefile.
    Updated Readme.
    Simplified sample page.

--- sample/index.php
+++ sample/index.php
@@ -1,23 +1,17 @@
 <html>
 <head>
 	<title>Hello world!</title>
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
 	body {
 		background-color: white;
 		text-align: center;
 		padding: 50px;
-		font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
-	}
-
-	#logo {
-		margin-bottom: 40px;
+		font-family: Helvetica,Arial,sans-serif;
 	}
 	</style>
 </head>
 <body>
-	<img id="logo" src="logo.png" />
-	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
+	<h1><?php echo "Hello ".(isset($_ENV["NAME"])?$_ENV["NAME"]:"world")."!"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
@@ -42,12 +36,6 @@
 		}
 		?>
 	<?php
-	}
-
-	if($_ENV["TUTUM_AUTH"]) {
-		?>
-		<h3>I have Tutum API powers!</h3>
-		<?php
 	}
 	?>
 </body>
